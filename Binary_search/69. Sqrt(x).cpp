338928b8da31020d5ec4f9cf1170781de8d9c82e
Vipul Kumar <74072895+Cyber321-glit-majer@users.noreply.github.com>
2022-09-19 16:36:50 +0530

    Create 69. Sqrt(x).cpp
    Given a non-negative integer x, compute and return the square root of x.
    
    Since the return type is an integer, the decimal digits are truncated, and only the integer part of the result is returned.
    
    Note: You are not allowed to use any built-in exponent function or operator, such as pow(x, 0.5) or x ** 0.5.
    
     
    
    Example 1:
    
    Input: x = 4
    Output: 2
    Example 2:
    
    Input: x = 8
    Output: 2
    Explanation: The square root of 8 is 2.82842..., and since the decimal part is truncated, 2 is returned.
     
    
    Constraints:
    
    0 <= x <= 231 - 1

---
+++ Binary_search/69. Sqrt(x).cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int binarys(int n)
+    {
+        int s=0;
+        int e=n;
+         long long int mid=s+(e-s)/2;
+        long long int ans=-1;
+        while(s<=e)
+        {
+            
+            long long int sq=mid*mid;
+            if(sq==n)
+                return mid;
+            else if(sq<n)
+            {  ans=mid;
+                s=mid+1;
+            }
+            else
+            {
+              
+               e=mid-1; 
+            }
+           mid=s+(e-s)/2;
+        }
+        return ans;
+    }
+    int mySqrt(int x) {
+        
+      return binarys(x);
+        
+        
+    }
+};
